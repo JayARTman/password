05855f21b2c3d9d84b68d4efa970f8eddaf84ffc
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-20 14:25:46 -0500

    Prompt rework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,43 @@
 // Assignment code here
 
-//var getRandomSync =
+//Possible Character
+var lower = ('"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"');
+var upper = ('"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"');
+var numbers = ('"0","1","2","3","4","5","6","7","8","9"');
+var symbol = ('"!","\",""","#","$","%","&"');
 
-//Things to do
-//Steps create the function or variable to grab random letters, numbers and symbols.
-//Identify length of password
-//Use Math.random with floor to determine what to pick
-//
-//Send password to #password
-var length = prompt("How many characters will your password be? Min characters 8 max 128");
-var charType = prompt("Enter a character type: symbol, number, upper, lowercase");
-var symbol = prompt("Would you like to include symbols");
-var num = prompt("Would you like to include numbers?");
-var upper = prompt("would you like to include capital letters");
+var allChar = [];
 
-function generate(){
-var charSet = "";
-  var charTypeLower = charType.toLowerCase();
-  if( charTypeLower === "lowercase" ) {
-    charSet = "abcdefghijklmnopqrstuvwxyz";
-  } else if( charTypeLower === "uppercase" ) {
-    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( charTypeLower === "numeric" ) {
-    charSet = "0123456789";
-  } else if( charTypeLower === "special" ) {
-    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } 
+//Start Function
+function generatePassword() {
+  var lengthChar = window.prompt("How many characters will your password be?");
+  lengthChar = parseInt(lengthChar);
+  if (lengthChar < 8 || lengthChar > 128) {
+    alert("Please enter a password length between 8 and 128")
+    return;
+  }
+  console.log(lengthChar);
 
+  var yesLowercase = confirm("Would you like to include lower case letters?");
+  var yesUppercase = confirm("Would you like to include upper case letters?");
+  var yesNumber = confirm("Would you like to include numbers?");
+  var yesSymbol = confirm("Would you like to include symbols?");
+  
   //return value
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
-    password += charSet.charAt(Math.floor(Math.random() * charSet.length));
-}
-
-}
+  //var password = "";
+  //for (var i = 0; i < length; i++) {
+  //picks a character within charSet at index of random number
+  //password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
-  var password = generate();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
