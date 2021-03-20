0c8a10cb38e34bc26c333f371709be517c2a2f53
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-20 09:56:28 -0500

    Turn functions into Variables and add To Do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 // Assignment code here
-console.clear();
-
 
 //var getRandomSync =
-var getRandomUpper String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-var getRandomUpper String.fromCharCode(Math.floor(Math.Random() * 26) + 65);
-var getRandomNumber String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-var getRandomSymbol const symbols = "!@#$%^&*(){}[]=<>/,." [Math.floor(Math.random() * symbols.length)];
 
-console.log(getRandomNumber)
+//Things to do
+//Steps create the function or variable to grab random letters, numbers and symbols.
+//Identify length of password
+//Use Math.random with floor to determine what to pick
+//
+//Send password to #password
+
+
+var lowerCase ='"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"';
+var upperCase ='"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"';
+var numbers = ["1","2","3","4","5","6","7","8","9"]
+var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","?","<",">","."]
 
 
 
